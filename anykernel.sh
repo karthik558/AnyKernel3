1fe54b73bfdf9abd30525c8549e5880b096e9094
Panchajanya1999 <rsk52959@gmail.com>
2019-09-04 14:50:25 +0530

    anykernel.sh: Skip hash check
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit 69b3edd2147a0f069540b89d0f105a1e4ab5837e)

--- anykernel.sh
+++ anykernel.sh
@@ -60,13 +60,6 @@ append_file fstab.tuna "usbdisk" fstab;
 if mountpoint -q /data; then
   # Optimize F2FS extension list (@arter97)
   for list_path in $(find /sys/fs/f2fs* -name extension_list); do
-    hash="$(md5sum $list_path | cut -d' ' -f1)"
-
-    # Skip update if our list is already active
-    if [[ $hash == "0e2627998f2f06aa951f90d2af30e859" ]]; then
-      echo "Extension list up-to-date: $list_path"
-      continue
-    fi
 
     ui_print "  • Optimizing F2FS extension list"
     echo "Updating extension list: $list_path"
