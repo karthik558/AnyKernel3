399e18152a9068618c6fe9635dd06b34e0618c22
Panchajanya1999 <panchajanya@azure-dev.live>
2019-09-30 22:58:30 +0530

    anykernel: Don't set permission for non-existent ramdisk dir
    We aren't going to do any retarded ramdisk modifications. So just don't chown ramdisk dir or do not do any attempt to find it.
    Fix warnings like
    
    chown: /tmp/anykernel/ramdisk/*: No such file or directory
    chown: /tmp/anykernel/ramdisk/*: No such file or directory
    find: /tmp/anykernel/ramdisk/*: No such file or directory
    find: /tmp/anykernel/ramdisk/*: No such file or directory
    chown: /tmp/anykernel/ramdisk/init*: No such file or directory
    chown: /tmp/anykernel/ramdisk/init*: No such file or directory
    find: /tmp/anykernel/ramdisk/init*: No such file or directory
    find: /tmp/anykernel/ramdisk/init*: No such file or directory
    chown: /tmp/anykernel/ramdisk/sbin: No such file or directory
    chown: /tmp/anykernel/ramdisk/sbin: No such file or directory
    find: /tmp/anykernel/ramdisk/sbin: No such file or directory
    find: /tmp/anykernel/ramdisk/sbin: No such file or directory
    
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- anykernel.sh
+++ anykernel.sh
@@ -36,8 +36,8 @@ magisk_check;
 if [ "$magisk_present" = true ]; then
   # AnyKernel file attributes
   # set permissions/ownership for included ramdisk files
-  set_perm_recursive 0 0 755 644 $ramdisk/*;
-  set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+  #set_perm_recursive 0 0 755 644 $ramdisk/*;
+  #set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
   dump_boot;
 
