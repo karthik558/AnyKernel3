29cfa0a2bc76bc773403aba02df43f31799468b1
Panchajanya1999 <panchajanya@azure-dev.live>
2021-02-16 20:50:21 +0530

    anykernel: Add support for UCLAMP
    Change-Id: Ibc3f164f78e65d01495569dc0f15c6f4fb0908d9
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- anykernel.sh
+++ anykernel.sh
@@ -68,6 +68,38 @@ if mountpoint -q /data; then
   done
 fi
 
+# Uclamp tunables
+if [ -f /dev/cpuset/top-app/uclamp.max ]; then
+	ui_print "  • Uclamp supported kernel"
+	#Uclamp tuning
+	sysctl -w kernel.sched_util_clamp_min_rt_default=500
+
+	#top-app
+	echo max > /dev/cpuset/top-app/uclamp.max
+	echo 10  > /dev/cpuset/top-app/uclamp.min
+	echo 1   > /dev/cpuset/top-app/uclamp.boosted
+	echo 1   > /dev/cpuset/top-app/uclamp.latency_sensitive
+
+	#foreground
+	echo 50 > /dev/cpuset/foreground/uclamp.max
+	echo 0  > /dev/cpuset/foreground/uclamp.min
+	echo 0  > /dev/cpuset/foreground/uclamp.boosted
+	echo 0  > /dev/cpuset/foreground/uclamp.latency_sensitive
+
+	#background
+	echo max > /dev/cpuset/background/uclamp.max
+	echo 20  > /dev/cpuset/background/uclamp.min
+	echo 0   > /dev/cpuset/background/uclamp.boosted
+	echo 0   > /dev/cpuset/background/uclamp.latency_sensitive
+
+	#system-background
+	echo 40 > /dev/cpuset/system-background/uclamp.max
+	echo 0  > /dev/cpuset/system-background/uclamp.min
+	echo 0  > /dev/cpuset/system-background/uclamp.boosted
+	echo 0  > /dev/cpuset/system-background/uclamp.latency_sensitive
+
+fi
+
 write_boot;
 ## end install
 
